d052f30bb3a973f60f658ed4395ea59aa1464fb8
Ogi Dobrev <ogidobrev2010@gmai..com>
2026-07-27 00:42:40 +0300

    bump sw cache, add version badge v1.0.1, no-cache headers

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'helious-v1';
+const CACHE = 'helious-v2';
 const URLS = ['/', '/index.html', '/forecast.js', '/manifest.json'];
 
 self.addEventListener('install', e => {
@@ -7,11 +7,15 @@ self.addEventListener('install', e => {
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(clients.claim());
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => clients.claim())
+  );
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() => r))
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
